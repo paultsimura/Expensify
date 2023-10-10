640d051b39b7940d7d033a34a67cbe8406f0968e
Pavlo Tsimura <paultsimura@gmail.com>
2023-10-10 14:00:39 +0200

    Simplify doubleClick handling of ImageZoom

--- src/components/ImageView/index.native.js
+++ src/components/ImageView/index.native.js
@@ -1,13 +1,14 @@
 import React, {useState, useRef, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import {View, PanResponder} from 'react-native';
+import {View} from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
-import _ from 'underscore';
 import styles from '../../styles/styles';
 import variables from '../../styles/variables';
 import FullscreenLoadingIndicator from '../FullscreenLoadingIndicator';
 import Image from '../Image';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
+
+const MAX_ZOOM_SCALE = 20;
 
 /**
  * On the native layer, we use a image library to handle zoom functionality
@@ -35,10 +36,6 @@ const defaultProps = {
     style: {},
 };
 
-// Use the default double click interval from the ImageZoom library
-// https://github.com/ascoders/react-native-image-zoom/blob/master/src/image-zoom/image-zoom.type.ts#L79
-const DOUBLE_CLICK_INTERVAL = 175;
-
 function ImageView({isAuthTokenRequired, url, onScaleChanged, onPress, style}) {
     const {windowWidth, windowHeight} = useWindowDimensions();
 
@@ -47,35 +44,12 @@ function ImageView({isAuthTokenRequired, url, onScaleChanged, onPress, style}) {
         width: 0,
         height: 0,
     });
+    const [doubleTapScale, setDoubleTapScale] = useState(2);
+
     const [containerHeight, setContainerHeight] = useState(null);
 
     const imageZoomScale = useRef(1);
-    const lastClickTime = useRef(0);
-    const numberOfTouches = useRef(0);
     const zoom = useRef(null);
-
-    /**
-     * Updates the amount of active touches on the PanResponder on our ImageZoom overlay View
-     *
-     * @param {Event} e
-     * @param {GestureState} gestureState
-     * @returns {Boolean}
-     */
-    const updatePanResponderTouches = (e, gestureState) => {
-        if (_.isNumber(gestureState.numberActiveTouches)) {
-            numberOfTouches.current = gestureState.numberActiveTouches;
-        }
-
-        // We don't need to set the panResponder since all we care about is checking the gestureState, so return false
-        return false;
-    };
-
-    // PanResponder used to capture how many touches are active on the attachment image
-    const panResponder = useRef(
-        PanResponder.create({
-            onStartShouldSetPanResponder: updatePanResponderTouches,
-        }),
-    ).current;
 
     /**
      * When the url changes and the image must load again,
@@ -137,44 +111,15 @@ function ImageView({isAuthTokenRequired, url, onScaleChanged, onPress, style}) {
         imageZoomWidth = Math.min(imageZoomWidth, roundedContainerWidth * maxDimensionsScale);
         imageZoomHeight = Math.min(imageZoomHeight, roundedContainerHeight * maxDimensionsScale);
 
+        const minImageScale = Math.min(imageZoomWidth, imageZoomHeight);
+        const maxImageScale = Math.max(imageZoomWidth, imageZoomHeight);
+        setDoubleTapScale(Math.min(MAX_ZOOM_SCALE, Math.max(maxImageScale / minImageScale, 2)));
+
         setImageDimensions({
             height: imageZoomHeight,
             width: imageZoomWidth,
         });
         setIsLoading(false);
-    };
-
-    const configurePanResponder = () => {
-        const currentTimestamp = new Date().getTime();
-        const isDoubleClick = currentTimestamp - lastClickTime.current <= DOUBLE_CLICK_INTERVAL;
-        lastClickTime.current = currentTimestamp;
-
-        // Let ImageZoom handle the event if the tap is more than one touchPoint or if we are zoomed in
-        if (numberOfTouches.current === 2 || imageZoomScale.current !== 1) {
-            return true;
-        }
-
-        // When we have a double click and the zoom scale is 1 then programmatically zoom the image
-        // but let the tap fall through to the parent so we can register a swipe down to dismiss
-        if (isDoubleClick) {
-            zoom.current.centerOn({
-                x: 0,
-                y: 0,
-                scale: 2,
-                duration: 100,
-            });
-
-            // onMove will be called after the zoom animation.
-            // So it's possible to zoom and swipe and stuck in between the images.
-            // Sending scale just when we actually trigger the animation makes this nearly impossible.
-            // you should be really fast to catch in between state updates.
-            // And this lucky case will be fixed by migration to UI thread only code
-            // with gesture handler and reanimated.
-            onScaleChanged(2);
-        }
-
-        // We must be either swiping down or double tapping since we are at zoom scale 1
-        return false;
     };
 
     // Default windowHeight accounts for the modal header height
@@ -194,12 +139,30 @@ function ImageView({isAuthTokenRequired, url, onScaleChanged, onPress, style}) {
             {Boolean(containerHeight) && (
                 <ImageZoom
                     ref={zoom}
+                    maxScale={MAX_ZOOM_SCALE}
                     onClick={onPress}
+                    onDoubleClick={() => {
+                        const scale = imageZoomScale.current === 1 ? doubleTapScale : 1;
+                        zoom.current.centerOn({
+                            scale,
+                            x: 0,
+                            y: 0,
+                            duration: 100,
+                        });
+
+                        // onMove will be called after the zoom animation.
+                        // So it's possible to zoom and swipe and stuck in between the images.
+                        // Sending scale just when we actually trigger the animation makes this nearly impossible.
+                        // you should be really fast to catch in between state updates.
+                        // And this lucky case will be fixed by migration to UI thread only code
+                        // with gesture handler and reanimated.
+                        onScaleChanged(scale);
+                    }}
+                    enableDoubleClickZoom={false}
                     cropWidth={windowWidth}
                     cropHeight={calculatedWindowHeight}
                     imageWidth={imageDimensions.width}
                     imageHeight={imageDimensions.height}
-                    onStartShouldSetPanResponder={configurePanResponder}
                     onMove={({scale}) => {
                         onScaleChanged(scale);
                         imageZoomScale.current = scale;
@@ -221,16 +184,6 @@ function ImageView({isAuthTokenRequired, url, onScaleChanged, onPress, style}) {
                         resizeMode={Image.resizeMode.contain}
                         onLoadStart={imageLoadingStart}
                         onLoad={configureImageZoom}
-                    />
-                    {/**
-                       Create an invisible view on top of the image so we can capture and set the amount of touches before
-                      the ImageZoom's PanResponder does. Children will be triggered first, so this needs to be inside the
-                      ImageZoom to work
-                      */}
-                    <View
-                        /* eslint-disable-next-line react/jsx-props-no-spreading */
-                        {...panResponder.panHandlers}
-                        style={[styles.w100, styles.h100, styles.invisible]}
                     />
                 </ImageZoom>
             )}
